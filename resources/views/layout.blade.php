049db913c971a055b636167d7e7e16bd2847efb7
demo571 <jm9366872@gmail.com>
2025-05-12 11:11:43 +0530

    feat:remove proloader

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -50,17 +50,8 @@
 </head>
 
 <body>
-    <!-- tap to top-->
-    {{-- <div class="tap-top">
-        <svg class="feather">
-            <use href="https://admin.pixelstrap.net/edmin/assets/svg/feather-icons/dist/feather-sprite.svg#arrow-up">
-            </use>
-        </svg>
-    </div> --}}
-    <!-- loader-->
-    <div class="loader-wrapper">
-        <div class="loader"></div>
-    </div>
+
+
     <main class="page-wrapper compact-wrapper" id="pageWrapper">
         <!-- Page header start -->
 
