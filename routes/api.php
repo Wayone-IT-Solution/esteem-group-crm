4f5514beed0c443c3ae4517cf02cb562824c1b11
Bhavesh <bhaveshkapoor09@gmail.com>
2025-07-01 15:28:33 +0530

    feat:fix name

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post("/v1/webhook", [ApiController::class, 'webhook']);
 Route::post("/v1/webhook/we-care-auto-repairs-add-pannel-bt", [ApiController::class, 'pannelBt']);
-Route::post("/v1/webhook/esteencars", [ApiController::class, 'esteemcars']);
+Route::post("/v1/webhook/esteemcars", [ApiController::class, 'esteemcars']);
 
 Route::get('/v1/bot',[ApiController::class, 'createLeadFromBot']);
